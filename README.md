90c9d422ceb6fc5db9e5bc2c5c82555b6203205f
Jewels <107947731+marked9999@users.noreply.github.com>
2023-11-11 12:17:01 -0500

    added sockets

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 <p>1 (Line 8): This code accesses our main.py file to find the path for the static css file. </p>
 <p>2 (Line 17): This is called a block. Blocks are areas in HTML code that can have other code injected into them. 
 In this case, our home.html and room.html files.</p>
+
+#### Main.py
+<p>1 (Line 18): What this is saying is: if name is empty, do:</p>
+<p>2 (Line 21): What this is saying is: if trying to join and code is empty, do:</p>
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase, ascii_lowercase
@@ -7,9 +7,87 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "3789"
 socketio = SocketIO(app)
 
+rooms = {}
+
+def GenerateUniqueCode(Length):
+    while True:
+        code = ""
+        for _ in range(Length):
+            code += random.choice(ascii_uppercase)
+        if code not in rooms:
+            break
+        
+    return code
+
 @app.route("/", methods=["POST", "GET"])
 def home():
+    session.clear()
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+        
+        if not name:
+            return render_template("home.html", error="Please enter a name.", code=code, name=name)
+        
+        if join is False and not code:
+            return render_template("home.html", error="Please enter a room code.", code=code, name=name)
+        
+        room = code
+        if create != False:
+            room = GenerateUniqueCode(4)
+            rooms[room] = {
+                "members": 0, 
+                "messages": []
+            }
+        elif code not in rooms:
+            return render_template("home.html", error="Room does not exist.", code=code, name=name)
+        
+        session["room"] = room
+        session["name"] = name
+        return redirect(url_for("room"))
+    
     return render_template("home.html")
+
+@app.route("/room")
+def room():
+    room = session.get("room")
+    if room is None or session.get("name") is None or room not in rooms:
+        return redirect(url_for("home"))
+    
+    return render_template("room.html")
+
+@socketio.on("connect")
+def connect(auth);
+    room = session.get("room")
+    name = session.get("name")
+    if not room or not name:
+        return
+    if room not in rooms:
+        leave_room(room)
+        return
+    
+    join_room(room)
+    send({"token": "[+] ", "name": name, "message": "has connected"}, to=room)
+    rooms[room]["members"] += 1
+    print(f"[+] {name} connected to {room}")
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+    
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+    
+    send({"token": "[-] ", "name": name, "message": "has disconnected"}, to=room)
+    print(f"[-] {name} has disconnected from {room}")
+
+
 
 if __name__  == "__main__":
     socketio.run(app, debug=True)
