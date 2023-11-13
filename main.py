62894197f428759196f6ff57a496b4752931a7d4
Jewels <jewelsytoffcial@gmail.com>
2023-11-13 16:21:05 -0500

    I'll fix it later ig

--- main.py
+++ main.py
@@ -104,6 +104,6 @@ def disconnect():
 
 
 if __name__  == "__main__":
-    socketio.run(app)
-    #socketio.run(app, host="0.0.0.0", port=5000)
+    #socketio.run(app)
+    socketio.run(app, host="0.0.0.0", port=5000)
     #socketio.run(app, allow_unsafe_werkzeug=True)
