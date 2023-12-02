c24654eb7c49b4d660d82e8ad10ba0d13bb20bbe
Jewels <jewelsytoffcial@gmail.com>
2023-12-02 17:54:27 -0500

    home.html is done

--- main-remake.py
+++ main-remake.py
@@ -1,3 +1,4 @@
+from ast import Dict
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random, datetime, sqlite3, json
@@ -7,7 +8,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "830156"
 socketio = SocketIO(app)
 
-def readJSON(path):
+def readJSON(path) -> dict or None:
     try:
         with open(path, 'r') as file:
             data = json.load(file)
@@ -29,23 +30,28 @@ class Generator():
             if code in data["houses"]:
                 break
         return code
-    def id(len):
+    def id(self, len):
         while True:
-            id = ""
+            id: int = 0
+            isthere: bool = False
             for _ in range(len):
                 id += random.randint(1, 9)
-            if id in data["users"]:
+            for user in data["users"]:
+                if data[user]["id"] == id:
+                    isthere = True
+            if isthere == True:
                 break
-        
-        return id
+            
+            return id
 class Watcher():
     def watch(censor: list):
         pass
     def censor():
         pass
 
-data = readJSON("./data.json")
+data: dict = readJSON("./data.json")
 cookies = {}
+gen = Generator()
 
 @app.route("/", methods=["POST", "GET"])
 def home():
@@ -55,21 +61,32 @@ def home():
         login = request.form.get("login", False)
         signup = request.form.get("signup", False)
         
-        if username and password != None:
-            if login != False:
-                isthere = False
-                for user in data["users"]:
-                    if username == user["name"]:
-                        isthere = False
-                        break
-                    else:
-                        continue
-                if isthere == True:
-                    redirect(url_for("app"))
-                else:
-                    pass
-            elif signup != False:
-                pass
+        if not username or not password:
+            return render_template("home.html", error="Enter all info.", username=username, password=password)
+
+        if signup != False:
+            if username in data["users"]:
+                return render_template("home.html", error="Username is in use.", username=username, password=password)
+            data["users"][username] = {
+                "id": gen.id(4),
+                "password": password
+            }
+            print(f"Created account {username}: {password}")
+        
+        if username not in data["users"] or data["users"][username]["password"] != password:
+            return render_template("home.html", error="Incorrect user info.", username=username, password=password)
+                
+        
+        session["username"] = username
+        session["password"] = password
+        saveJSON("data.json", data)
+        return redirect(url_for("room"))
+    
+    return render_template("home.html")
+
+@app.route("/app")
+def room():
+    return render_template("room.html")
 
 if __name__ == "__main__":
     socketio.run(app)
